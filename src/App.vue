50ffda8f8561d3838837a4e016aabf32fa3d4bbe
BeerHobbyist <waluzenicz.ignacy2@gmail.com>
2024-03-25 22:46:35 +0100

    fix: fix multipe gets bug

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts">
+import { onMounted } from 'vue';
 import AppBar from './components/AppBar.vue'
+import { productsStore } from './products';
+
+
+onMounted(() => {
+    productsStore.fetchProducts();
+});
 </script>
 
 <template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,27 +1,23 @@
 <script setup lang="ts">
-import AppBar from '../components/AppBar.vue'
 import DisplayItem from '../components/DisplayItem.vue'
 import { productsStore } from '../products'
 import { onMounted } from 'vue'
 
 
-onMounted(async () => {
-  productsStore.fetchProducts();
-});
-
 </script>
 
 <template>
-  <div class="item-display">
-    <DisplayItem v-for="product in productsStore.products" :key="product.id" :id="product.id" :name="product.productName" :packageType="product.packagingType" />
-  </div>
+    <div class="item-display">
+        <DisplayItem v-for="product in productsStore.products" :key="product.id" :id="product.id"
+            :name="product.productName" :packageType="product.packagingType" />
+    </div>
 </template>
 
 <style scoped>
 .item-display {
-  display: flex;
-  flex-wrap: wrap;
-  flex-direction: row;
-  justify-content: center;
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    justify-content: center;
 }
 </style>
