f93a49746e1de4d9c4d8addb192d1e266f7a21e0
BeerHobbyist <waluzenicz.ignacy2@gmail.com>
2024-03-25 23:02:39 +0100

    feat: add simple cart view

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,7 +1,19 @@
 <script setup lang="ts">
+import { cartStore } from '@/cart';
+import { productsStore } from '@/products';
+
+const getName = (id: number) => {
+    return productsStore.products.find((product) => product.id === id)?.productName;
+};
 
 </script>
 
 <template>
-    <h1>Cart</h1>
+    <h1>Cart:</h1>
+    <ul>
+        <li v-for="item in cartStore.cartItems" :key="item.id">
+            <p>{{ getName(item.id) }}: {{ item.count }}</p>
+            <p></p>
+        </li>
+    </ul>
 </template>
