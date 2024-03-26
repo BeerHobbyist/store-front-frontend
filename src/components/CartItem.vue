d7d2e18af7a5b44e5438d9334aa2d9b702fa53ba
BeerHobbyist <waluzenicz.ignacy2@gmail.com>
2024-03-26 16:30:58 +0100

    feat: add cart item styling

---
+++ src/components/CartItem.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+defineProps<{ selectedAmount: number, name?: string }>();
+
+</script>
+
+<template>
+    <div class="cartItem">
+        <p> {{ name }} </p>
+        <p> {{ selectedAmount }} </p>
+    </div>
+</template>
+
+<style scoped>
+.cartItem {
+    border: 1px solid #ccc;
+    background-color: gray;
+    border-radius: 10px;
+    margin: 1px;
+    justify-content: center;
+    flex-direction: row;
+    display: flex;
+    text-align: center;
+    width: 100%;
+}
+</style>
--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { cartStore } from '@/cart';
 import { productsStore } from '@/products';
+import CartItem from '../components/CartItem.vue'
 
 const getName = (id: number) => {
     return productsStore.products.find((product) => product.id === id)?.productName;
@@ -10,10 +11,20 @@ const getName = (id: number) => {
 
 <template>
     <h1>Cart:</h1>
-    <ul>
-        <li v-for="item in cartStore.cartItems" :key="item.id">
-            <p>{{ getName(item.id) }}: {{ item.count }}</p>
-            <p></p>
-        </li>
-    </ul>
+    <div class="itemList">
+        <CartItem v-for="item in cartStore.cartItems" :key="item.id" :selectedAmount="item.count" :name="getName(item.id)" />
+    </div>
 </template>
+
+<style scoped>
+.itemList {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center; 
+    border: 2px solid #000;
+    padding: 10px; 
+    margin: 10px;
+    width: 50%;
+}
+</style>
