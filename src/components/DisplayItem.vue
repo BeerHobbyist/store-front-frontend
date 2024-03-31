67f0e4b90ec01b717d3d32d1b59d80a2d3b94e1a
BeerHobbyist <waluzenicz.ignacy2@gmail.com>
2024-03-31 23:22:19 +0200

    feat: home screen UI color change

--- src/components/DisplayItem.vue
+++ src/components/DisplayItem.vue
@@ -37,8 +37,8 @@ watch(selectedAmount, (newValue: number) => {
 }
 
 .item {
-    border: 1px solid #ccc;
-    background-color: var(--secondary-color);
+    box-shadow: 0px 0px 2px 0px var(--text-color); /* Adds a shadow to the container */
+    background-color: white;
     border-radius: 10px;
     padding: 10px;
     margin: 10px;
@@ -69,23 +69,26 @@ watch(selectedAmount, (newValue: number) => {
     border-radius: 5px 0px 0px 5px;
     padding: 5px;
     margin: 0ch;
+    border: 1px solid var(--neutral-gray);
+    border-radius: 5px;
 }
 
 .counter button {
     width: 35px;
     height: 35px;
     border: none;
-    border-radius: 0px 5px 5px 0px;
-    background-color: var(--light-primary);
+    border-radius: 5px;
+    margin-left: 5px;
+    background-color: var(--accent-color);
     color: white;
     font-size: 1.5rem;
     cursor: pointer;
-    transition: background-color 0.3s ease;
+    transition: background-color 0.3s, transform 0.2s; /* Smooth transition for hover effects */
 }
 
 .counter button:hover {
-    background-color: var(--primary-color);
-}
+    transform: scale(1.05); /* Slightly enlarge the button on hover */
+  }
 
 .counter img {
     padding: 7px 0px 0px 0px;
