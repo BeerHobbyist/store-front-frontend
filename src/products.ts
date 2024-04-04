5d1a8f3910f1f26f6f526ac150ec51f6d3c05081
BeerHobbyist <waluzenicz.ignacy2@gmail.com>
2024-04-04 13:49:50 +0200

    chore: add guard funtion to check for type mismatches

--- src/products.ts
+++ src/products.ts
@@ -7,15 +7,28 @@ type Product = {
     price: number;
 }
 
+function isProduct(item: any): item is Product {
+    return (
+        item &&
+        'id' in item && typeof item.id === 'string' &&
+        'name' in item && typeof item.name === 'string' &&
+        'packageType' in item && typeof item.packageType === 'string' &&
+        'price' in item && typeof item.price === 'number'
+    );
+}
 
 export const productsStore = reactive({
     products: [] as Product[],
     fetchProducts: async () => {
         try {
             const response = await fetch('http://127.0.0.1:5001/belex-storefront/us-central1/getProducts');
-            const data: Product[] = await response.json();
-            data.forEach((product: Product) => {
-                productsStore.products.push(product);
+            const data: any[] = await response.json();
+            data.forEach((item: any) => {
+                if (isProduct(item)) {
+                    productsStore.products.push(item);
+                } else {
+                    console.error('Fetched item does not match the Product type:', item);
+                }
             });
         } catch (error) {
             console.error(error);
