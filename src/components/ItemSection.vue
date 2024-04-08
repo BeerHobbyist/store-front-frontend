91c579fe440cc7420aab59cdedb8bd19a9bd3b70
BeerHobbyist <waluzenicz.ignacy2@gmail.com>
2024-04-08 23:56:12 +0200

    chore: center item section

--- src/components/ItemSection.vue
+++ src/components/ItemSection.vue
@@ -5,11 +5,11 @@ defineProps<{ sectionName: string, items: Array<Product> }>();
 </script>
 
 <template>
-    <div class="flex flex-wrap flex-col">
-        <div class="p-4">
+    <div class="flex flex-wrap flex-col items-center">
+        <div class="p-4 w-6/12">
             <span class="text-2xl font-sans uppercase border-b border-blue-400 block pb-2">{{ sectionName }}</span>
         </div>
-        <div class="flex flex-row justify-start ">
+        <div class="flex flex-row justify-center ">
             <DisplayItem v-for="item in items" :key="item.id" :id="item.id" :name="item.name"
                 :packageType="item.packageType" :price="item.price" :imageUrl="item.imageUrl" />
         </div>
