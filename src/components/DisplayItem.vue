b0a8a9790456da919a00304e9aa4bc236021b201
BeerHobbyist <waluzenicz.ignacy2@gmail.com>
2024-04-10 18:11:14 +0200

    feat: changed anims

--- src/components/DisplayItem.vue
+++ src/components/DisplayItem.vue
@@ -15,8 +15,8 @@ watch(selectedAmount, (newValue: number) => {
 </script>
 
 <template>
-    <div v-motion-slide-visible-once-bottom
-        class="flex flex-col items-center bg-white ring-cyan-600 shadow-lg rounded-lg w-52 m-4 p2 transition ease-in-out duration-300 hover:shadow-2xl">
+    <div
+        class="flex flex-col items-center bg-white ring-cyan-600 shadow-lg rounded-lg h-[310px] w-52 m-4 p2 transition ease-in-out duration-300 hover:shadow-2xl">
         <img class="w-40 m-1" :src="imageUrl">
         <div class="m-1 text-center ">
             <div>{{ props.name }}</div>
--- src/components/ItemSection.vue
+++ src/components/ItemSection.vue
@@ -5,7 +5,7 @@ defineProps<{ sectionName: string, items: Array<Product> }>();
 </script>
 
 <template>
-    <div class="flex flex-wrap flex-col items-center">
+    <div class="flex flex-wrap flex-col items-center" v-motion-slide-visible-once-bottom>
         <div class="p-4 w-6/12">
             <span class="text-2xl font-sans uppercase border-b border-blue-400 block pb-2">{{ sectionName }}</span>
         </div>
