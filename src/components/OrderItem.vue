39134670d4e53c11eee821b6948581d10365579c
BeerHobbyist <waluzenicz.ignacy2@gmail.com>
2024-04-20 22:19:06 +0200

    feat: add admin table

--- src/components/OrderItem.vue
+++ src/components/OrderItem.vue
@@ -12,9 +12,15 @@ onMounted(() => {
 </script>
 
 <template>
-    <div class="w-full">
+    <div class="w-full flex justify-between bg-neutral-100 p-2 mb-4 rounded-lg font-sans drop-shadow-md">
         <div>
-            <h1>{{ products.product.name }}</h1>
+            <h1 class="text-lg font-semibold text-gray-800">{{ products.product.name }}</h1>
+            <p class="text-gray-500">{{ products.product.packageType }}</p>
+        </div>
+        <div>
+            <p class="text-gray-800 text-right font-semibold">Ilość {{ products.count }} szt.</p>
+            <p class="text-gray-500 text-right">w cenie: {{ products.product.price }} zł/szt.</p>
         </div>
     </div>
+    
 </template>
--- src/components/OrdersTable.vue
+++ src/components/OrdersTable.vue
@@ -4,22 +4,25 @@ import { ordersStore } from "../orders";
 import OrderTab from "./OrderTab.vue";
 import OrderItem from "./OrderItem.vue";
 import { TabGroup, TabList, Tab, TabPanel, TabPanels } from "@headlessui/vue";
-import type { OrderedProduct } from "../orders";
 </script>
 
 <template>
-  <div class="w-full px-4 mt-8 my-auto">
+  <div class="w-4/6 mx-auto px-4 mt-8 my-auto">
     <TabGroup>
       <div class="flex rounded-lg h-[80vh]">
-        <TabList class="flex flex-col space-y-4 p-4">
+        <TabList
+          class="flex flex-col flex-shrink-0 overflow-y-auto space-y-4 p-4"
+        >
           <Tab
             as="template"
-            v-for="order in ordersStore.orders" :key="order.id"
+            v-for="order in ordersStore.orders"
+            :key="order.id"
             v-slot="{ selected }"
           >
             <OrderTab
               :class="{
-                'p-2 bg-white rounded-lg drop-shadow-md focus:outline-none transform ease-in-out duration-400': !selected,
+                'p-2 bg-white rounded-lg drop-shadow-md focus:outline-none transform ease-in-out duration-400':
+                  !selected,
                 'p-2 bg-blue-100 rounded-lg drop-shadow-xl scale-105 focus:outline-none':
                   selected,
               }"
@@ -32,11 +35,15 @@ import type { OrderedProduct } from "../orders";
             />
           </Tab>
         </TabList>
-        <TabPanels class="space-y-4 p-4 m-4 rounded-lg bg-white drop-shadow-lg w-full">
+        <TabPanels
+          class="p-4 m-4 rounded-lg bg-white drop-shadow-lg w-full overflow-y-auto flex-grow"
+        >
           <TabPanel v-for="order in ordersStore.orders" :key="order.id">
             <div v-for="product in order.productsOrdered">
               <OrderItem :products="product" />
             </div>
+            <h1 class="font-sans font-semibold text-lg mt-8">Komentarz do zamówenia:</h1>
+            <p class="font-sans">{{ order.remarks }}</p>
           </TabPanel>
         </TabPanels>
       </div>
