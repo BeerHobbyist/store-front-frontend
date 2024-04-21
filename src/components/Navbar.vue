2b4e226abdf1bc987d14b5f104300f7bbcea9b7a
BeerHobbyist <waluzenicz.ignacy2@gmail.com>
2024-04-21 22:29:52 +0200

    refactor: make icons as separate components

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,58 +1,22 @@
 <script setup lang="ts">
 import DefaultRouterLink from "@/components/DefaultRouterLink.vue";
+import ListIcon from "./Icons/ListIcon.vue";
+import CartIcon from "./Icons/CartIcon.vue";
+import ContactIcon from "./Icons/ContactIcon.vue";
 </script>
 
 <template>
   <nav>
     <DefaultRouterLink to="/">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        class="w-6 h-6 mr-2"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M3.75 12h16.5m-16.5 3.75h16.5M3.75 19.5h16.5M5.625 4.5h12.75a1.875 1.875 0 0 1 0 3.75H5.625a1.875 1.875 0 0 1 0-3.75Z"
-        />
-      </svg>
+      <ListIcon />
       Oferta
     </DefaultRouterLink>
     <DefaultRouterLink to="/cart">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        class="w-6 h-6 mr-2"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
-        />
-      </svg>
+      <CartIcon />
       Koszyk
     </DefaultRouterLink>
     <DefaultRouterLink to="/contact">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        class="w-7 h-7 mr-2"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-        />
-      </svg>
+      <ContactIcon />
       Kontakt
     </DefaultRouterLink>
   </nav>
