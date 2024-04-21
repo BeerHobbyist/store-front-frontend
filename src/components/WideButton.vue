fc07f8b7e6f298395e048d6f5ae2771eeb6aad43
BeerHobbyist <waluzenicz.ignacy2@gmail.com>
2024-04-21 18:59:33 +0200

    refactor: fix formatting

--- src/components/WideButton.vue
+++ src/components/WideButton.vue
@@ -1,17 +1,19 @@
 <template>
   <button
     class="bg-blue-400 hover:bg-blue-500 rounded-3xl min-w-24 text-white shadow-lg transition ease-in-out duration-300 transform hover:scale-105"
-    @click="onClick">
-    <slot></slot> <!-- Allows for custom button text -->
+    @click="onClick"
+  >
+    <slot></slot>
+    <!-- Allows for custom button text -->
   </button>
 </template>
 
 <script setup lang="ts">
 // Props for custom behavior like emitting an event upon click
-const emit = defineEmits(['click']);
+const emit = defineEmits(["click"]);
 
 const onClick = () => {
-  emit('click'); // Emit a click event when the button is clicked
+  emit("click"); // Emit a click event when the button is clicked
 };
 </script>
 
